1362e3862cb1a8ac8bb1d37dd330e0f7ff882f02
Nat Epstein <nathaniel.epstein@gmail.com>
2022-08-30 17:23:12 -0400

    quotes fetched and logged to console. randomly picks quotes

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Working!")
+// Get Quotes From API
+let apiQuotes = [];
+
+function newQuote() {
+   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+async function getQuotes() {
+    
+        const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+        try {
+            const response = await fetch(apiUrl);
+            apiQuotes = await response.json();
+            // console.log(apiQuotes);
+            newQuote();
+        } catch(error) {
+            
+            //Handle Catch Error Here
+        }
+
+        
+}
+
+getQuotes();
+
